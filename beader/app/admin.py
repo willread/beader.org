c1bf3a64f427f9a2971843eae275f83558064ac6
Will Blanchette <will@unata.com>
2014-01-21 17:46:38 -0500

    Removed some deprecated files

--- beader/app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from app.models import Pattern
-import pytz
-
-admin.site.register(Pattern)
--- beader/app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from app.models import Pattern
-
-class PatternForm(forms.ModelForm):
-	class Meta:
-		model = Pattern

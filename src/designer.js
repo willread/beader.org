a0a3aa91a0788962d44132e603ddf7d72bffef07
William Blanchette <wb@wblanchette.com>
2016-12-14 11:58:23 -0700

    Added 'this'

--- src/designer.js
+++ src/designer.js
@@ -33,7 +33,7 @@ export class Designer {
     // Init pattern
 
     for(let ii = 0; ii < this.width * this.height; ii++){
-      pattern.push(clearColor);
+      this.pattern.push(clearColor);
     }
 
     // We're editing a pattern, load it from the backend

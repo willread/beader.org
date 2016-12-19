6b6d320609f8b64899d126fb68e178de2eac8ac2
William Blanchette <will@Williams-MBP.hitronhub.home>
2016-12-19 11:44:41 -0700

    Deprecated files from old version of the site

--- static/js/app/controllers/patterns.js
+++
@@ -1,8 +0,0 @@
-angular.module("Beader.controllers").controller("PatternsCtrl", ["$scope", "$http", function($scope, $http){
-        top.patternsScope = $scope;
-
-        $http.get("/patterns").
-            success(function(patterns){
-                $scope.patterns = patterns;
-            });
-    }]);

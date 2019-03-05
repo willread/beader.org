24aaf343aeb7a82350a68f69e8271cd04eed4a27
William Blanchette <will@Williams-MBP.hitronhub.home>
2019-03-05 15:15:19 -0700

    Adding build

---
+++ precache-manifest.46f4a43314d2a25e7b43de9ecbf19681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "16fe8ca00c4321810116c9f8872c6916",
+    "url": "/static/media/logo.16fe8ca0.png"
+  },
+  {
+    "revision": "48a0a6caa87bb88739361c693975be94",
+    "url": "/static/media/align-pixel.48a0a6ca.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "61e87df4c08ea673e328",
+    "url": "/static/js/main.61e87df4.chunk.js"
+  },
+  {
+    "revision": "63fa02546fb24ec8ee62",
+    "url": "/static/js/2.63fa0254.chunk.js"
+  },
+  {
+    "revision": "61e87df4c08ea673e328",
+    "url": "/static/css/main.9d10f781.chunk.css"
+  },
+  {
+    "revision": "6170dd606f15d2a9774b49ff3a085386",
+    "url": "/index.html"
+  }
+];

a1d698f271fd3b9b48da511f38b327c5ee662c99
William Blanchette <will@Williams-MBP.hitronhub.home>
2019-03-07 14:22:27 -0700

    Moved api to digitalocean

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const apiPath = 'https://beader-api.herokuapp.com';
+export const apiPath = 'https://beader-api.haqt.com';
 export const googleClientId = '204545753423-3igb69ajb3be6ftc6mu8ftkgmvqe3hcv.apps.googleusercontent.com';
 export const googleRedirectUri = 'https://beader.org';
 export const titleSuffix = ' | Beader';

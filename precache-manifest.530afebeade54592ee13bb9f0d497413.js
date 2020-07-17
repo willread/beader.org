c8ff2eca470bd6a206266057a2eb48d2e53cba35
Will Read <will@willread.ca>
2020-07-17 14:29:18 -0600

    Updates

--- precache-manifest.530afebeade54592ee13bb9f0d497413.js
+++ precache-manifest.530afebeade54592ee13bb9f0d497413.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "73aebc2f905777af5ccbfc8ad59bf3a9",
+    "revision": "00064800893458a8771d759cebb7428e",
     "url": "/index.html"
   },
   {
-    "revision": "df73e15c1c080e96675f",
-    "url": "/static/css/main.f9b257f8.chunk.css"
+    "revision": "1dd25a9ad1bbe97f86d6",
+    "url": "/static/css/main.fbc8fc2b.chunk.css"
   },
   {
-    "revision": "42edf165ff433be3eb83",
-    "url": "/static/js/2.aacb055b.chunk.js"
+    "revision": "e45018173fbb627c64e6",
+    "url": "/static/js/2.9039fbd8.chunk.js"
   },
   {
     "revision": "0e4c47b858ae6f79e735b78831b85dc8",
-    "url": "/static/js/2.aacb055b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.9039fbd8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "df73e15c1c080e96675f",
-    "url": "/static/js/main.400dee36.chunk.js"
+    "revision": "1dd25a9ad1bbe97f86d6",
+    "url": "/static/js/main.75b29263.chunk.js"
   },
   {
     "revision": "27480bc7bc4e28831305",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98a3c699337a9864a5b831ccd390c7bd.js"
+  "/precache-manifest.530afebeade54592ee13bb9f0d497413.js"
 );
 
 self.addEventListener('message', (event) => {

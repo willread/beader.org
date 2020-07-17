d60d1e0f24f9d022591ae42f9307dcdd44f752cc
Will Read <will@willread.ca>
2020-07-17 10:05:30 -0600

    Updates

---
+++ precache-manifest.530ffa8883cbcf6421c60107953facda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80714fa816975a9fdc757073046f92d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3283e88e9fb57e87320f",
+    "url": "/static/css/main.f9b257f8.chunk.css"
+  },
+  {
+    "revision": "9a84065379ff588a071b",
+    "url": "/static/js/2.88403718.chunk.js"
+  },
+  {
+    "revision": "0e4c47b858ae6f79e735b78831b85dc8",
+    "url": "/static/js/2.88403718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3283e88e9fb57e87320f",
+    "url": "/static/js/main.97048733.chunk.js"
+  },
+  {
+    "revision": "27480bc7bc4e28831305",
+    "url": "/static/js/runtime-main.65950f00.js"
+  },
+  {
+    "revision": "48a0a6caa87bb88739361c693975be94",
+    "url": "/static/media/align-pixel.48a0a6ca.png"
+  },
+  {
+    "revision": "16fe8ca00c4321810116c9f8872c6916",
+    "url": "/static/media/logo.16fe8ca0.png"
+  }
+]);
